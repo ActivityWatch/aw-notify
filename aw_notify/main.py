16675355c7f18f981eaa60672037964787ffe588
Erik Bjäreholt <erik@bjareho.lt>
2023-08-10 13:10:37 +0200

    feat: load categories from file, added yesterday summary, misc cleanup

--- aw_notify/main.py
+++ aw_notify/main.py
@@ -3,34 +3,36 @@ Get time spent for different categories in a day,
 and send notifications to the user on predefined conditions.
 """
 import logging
+import os
 import threading
+from collections import defaultdict
 from datetime import datetime, timedelta, timezone
 from functools import wraps
 from pathlib import Path
 from time import sleep
-from typing import Optional, Union
+from typing import (
+    Callable,
+    Optional,
+    TypeVar,
+    Union,
+)
 
 import aw_client.queries
 import click
+import tomlkit
 from desktop_notifier import DesktopNotifier
+from typing_extensions import TypeAlias
 
-logger = logging.getLogger(__name__)
-
-# TODO: Get categories from aw-webui export (in the future from server key-val store)
 # TODO: Add thresholds for total time today (incl percentage of productive time)
 
-# regex for productive time
-RE_PRODUCTIVE = r"Programming|nvim|taxes|Roam|Code"
+logger = logging.getLogger(__name__)
 TIME_OFFSET = timedelta(hours=4)
-
-
-CATEGORIES: list[tuple[list[str], dict]] = [
+FALLBACK_CATEGORIES: list[tuple[list[str], dict]] = [
     (
         ["Work"],
         {
             "type": "regex",
-            "regex": RE_PRODUCTIVE,
-            "ignore_case": True,
+            "regex": "Programming|nvim|taxes|Roam|Code",
         },
     ),
     (
@@ -38,7 +40,6 @@ CATEGORIES: list[tuple[list[str], dict]] = [
         {
             "type": "regex",
             "regex": r"Twitter|twitter.com|Home / X",
-            "ignore_case": True,
         },
     ),
     (
@@ -46,10 +47,69 @@ CATEGORIES: list[tuple[list[str], dict]] = [
         {
             "type": "regex",
             "regex": r"Youtube|youtube.com",
-            "ignore_case": True,
         },
     ),
 ]
+
+
+# TODO: move to aw-client utils
+# TODO: Get categories from aw-webui export (in the future from server key-val store)
+def load_category_toml(path: Path) -> list[tuple[list[str], dict]]:
+    with open(path, "r") as f:
+        toml = tomlkit.load(f)
+    return parse_category_toml(toml, parent=[])
+
+
+def parse_category_toml(toml: dict, parent: list[str]) -> list[tuple[list[str], dict]]:
+    """
+    Parse category config file and return a list of categories.
+    """
+    categories = []
+    if "categories" in toml:
+        toml = toml["categories"]
+    for cat_name, cat in toml.items():
+        if isinstance(cat, dict):
+            categories += parse_category_toml(cat, parent=parent + [cat_name])
+        else:
+            if cat_name == "$re":
+                categories.append((parent, {"type": "regex", "regex": cat}))
+            else:
+                categories.append(
+                    (parent + [cat_name], {"type": "regex", "regex": cat})
+                )
+    # create parent category with no rule if $re not given
+    if parent and parent not in (c for c, _ in categories):
+        categories.append((parent, {"type": "none"}))
+    return sorted(categories)
+
+
+def test_parse_category_toml():
+    """
+    Test parsing of category config file.
+    """
+    # Example category config file:
+    config = """
+    [categories]
+        [categories.Media]
+        Music = '[Ss]potify|[Ss]ound[Cc]loud|Mixxx|Shazam'
+            [categories.Media.Games]
+            '$re' = 'Video Games'
+            Steam = 'Steam'
+    """
+    categories = parse_category_toml(tomlkit.loads(config), parent=[])
+
+    # Check that "Media" category exists
+    assert categories[0][0] == ["Media"]
+
+    # Check that "Games" category exists, and has the correct regex
+    assert categories[1][0] == ["Media", "Games"]
+    assert categories[1][1]["regex"] == "Video Games"
+    assert categories[2][0] == ["Media", "Games", "Steam"]
+    assert categories[2][1]["regex"] == "Steam"
+
+
+CATEGORIES = FALLBACK_CATEGORIES
+
 
 time_offset = timedelta(hours=4)
 
@@ -58,36 +118,45 @@ aw = aw_client.ActivityWatchClient("aw-notify", testing=False)
 
 def cache_ttl(ttl: Union[timedelta, int]):
     """Decorator that caches the result of a function in-memory, with a given time-to-live."""
+    T = TypeVar("T")
+    CacheKey: TypeAlias = tuple
+
     _ttl: timedelta = ttl if isinstance(ttl, timedelta) else timedelta(seconds=ttl)
 
-    def wrapper(func):
+    def wrapper(func: Callable[..., T]):
+        last_update: dict[CacheKey, datetime] = defaultdict(
+            lambda: datetime(1970, 1, 1, tzinfo=timezone.utc)
+        )
+        cache: dict[CacheKey, T] = {}
+
         @wraps(func)
         def _cache_ttl(*args, **kwargs):
             now = datetime.now(timezone.utc)
-            if now - _cache_ttl.last_update > _ttl:
+            cache_key: CacheKey = (*args, *kwargs.items())
+            if now - last_update[cache_key] > _ttl:
                 logger.debug(f"Cache expired for {func.__name__}, updating")
-                _cache_ttl.last_update = now
-                _cache_ttl.cache = func(*args, **kwargs)
-            return _cache_ttl.cache
+                last_update[cache_key] = now
+                cache[cache_key] = func(*args, **kwargs)
+            return cache[cache_key]
 
-        _cache_ttl.last_update = datetime(1970, 1, 1, tzinfo=timezone.utc)
-        _cache_ttl.cache = None
         return _cache_ttl
 
     return wrapper
 
 
-@cache_ttl(timedelta(minutes=1))
-def get_time() -> dict[str, timedelta]:
+@cache_ttl(60)
+def get_time(date=None) -> dict[str, timedelta]:
     """
-    Returns a dict with the time spent today for each category.
+    Returns a dict with the time spent today (or for `date`) for each category.
     """
 
-    now = datetime.now(timezone.utc)
+    if date is None:
+        date = datetime.now(timezone.utc)
+    date = date.replace(hour=0, minute=0, second=0, microsecond=0)
     timeperiods = [
         (
-            now.replace(hour=0, minute=0, second=0, microsecond=0) + time_offset,
-            now,
+            date + time_offset,
+            date + time_offset + timedelta(days=1),
         )
     ]
 
@@ -108,10 +177,17 @@ def get_time() -> dict[str, timedelta]:
 
     res = aw.query(query, timeperiods)[0]
     res["cat_events"] += [{"data": {"$category": ["All"]}, "duration": res["duration"]}]
-    return {
-        ">".join(c["data"]["$category"]): timedelta(seconds=c["duration"])
-        for c in res["cat_events"]
-    }
+
+    top_level_only = True
+
+    cat_time: dict[str, timedelta] = defaultdict(timedelta)
+    for e in res["cat_events"]:
+        if top_level_only:
+            cat = e["data"]["$category"][0]
+        else:
+            cat = ">".join(e["data"]["$category"])
+        cat_time[cat] += timedelta(seconds=e["duration"])
+    return cat_time
 
 
 def to_hms(duration: timedelta) -> str:
@@ -144,7 +220,7 @@ def notify(title: str, msg: str):
     global notifier
     if notifier is None:
         notifier = DesktopNotifier(
-            app_name="ActivityWatch",
+            app_name="AW",
             app_icon=f"file://{icon_path}",
             notification_limit=10,
         )
@@ -169,7 +245,11 @@ class CategoryAlert:
     """
 
     def __init__(
-        self, category: str, thresholds: list[timedelta], label: Optional[str] = None
+        self,
+        category: str,
+        thresholds: list[timedelta],
+        label: Optional[str] = None,
+        positive=False,
     ):
         self.category = category
         self.label = label or category or "All"
@@ -177,6 +257,11 @@ class CategoryAlert:
         self.max_triggered: timedelta = timedelta()
         self.time_spent = timedelta()
         self.last_check = datetime(1970, 1, 1, tzinfo=timezone.utc)
+
+        # wether the alert is "positive"
+        # i.e. if the activity should be encouraged ("goal reached!")
+        # if not, assume neutral ("time spent")
+        self.positive = positive
 
     @property
     def thresholds_untriggered(self):
@@ -213,16 +298,22 @@ class CategoryAlert:
             pass
             # logger.debug("Not updating, too soon")
 
-    def check(self):
+    def check(self, silent=False):
         """Check if thresholds have been reached"""
         for thres in sorted(self.thresholds_untriggered, reverse=True):
             if thres <= self.time_spent:
                 # threshold reached
                 self.max_triggered = thres
-                notify(
-                    "Time spent",
-                    f"{self.label}: {to_hms(thres)} reached! ({to_hms(self.time_spent)})",
-                )
+                # TODO: use more general, or configurable, language for the notification
+                #       as each thres isn't necessarily a "goal" nor a "limit" being hit
+                if not silent:
+                    thres_str = to_hms(thres)
+                    spent_str = to_hms(self.time_spent)
+                    notify(
+                        "Goal reached!" if self.positive else "Time spent",
+                        f"{self.label}: {thres_str}"
+                        + (f"  ({spent_str})" if thres_str != spent_str else ""),
+                    )
                 break
 
     def status(self) -> str:
@@ -248,11 +339,18 @@ def main(verbose: bool):
     )
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
+    AW_CATEGORY_PATH = os.environ.get("AW_CATEGORY_PATH", None)
+    if AW_CATEGORY_PATH:
+        global CATEGORIES
+        CATEGORIES = load_category_toml(Path(AW_CATEGORY_PATH))
+        logger.info("Loaded categories from $AW_CATEGORY_PATH")
+
 
 @main.command()
 def start():
     """Start the notification service."""
-    checkin()
+    send_checkin()
+    send_checkin_yesterday()
     start_hourly()
     start_new_day()
     threshold_alerts()
@@ -262,12 +360,18 @@ def threshold_alerts():
     """
     Checks elapsed time for each category and triggers alerts when thresholds are reached.
     """
+    # TODO: make configurable
     alerts = [
         CategoryAlert("All", [td1h, td2h, td4h, td6h, td8h], label="All"),
         CategoryAlert("Twitter", [td15min, td30min, td1h], label="🐦 Twitter"),
         CategoryAlert("Youtube", [td15min, td30min, td1h], label="📺 Youtube"),
         CategoryAlert("Work", [td15min, td30min, td1h, td2h, td4h], label="💼 Work"),
     ]
+
+    # run through them once to check if any thresholds have been reached
+    for alert in alerts:
+        alert.update()
+        alert.check(silent=True)
 
     while True:
         for alert in alerts:
@@ -282,34 +386,44 @@ def threshold_alerts():
 
 
 @main.command()
-def _checkin():
-    """Send a summary notification."""
-    checkin()
-
-
 def checkin():
+    """Send a summary notification."""
+    send_checkin()
+
+
+def send_checkin(title="Time today", date=None):
     """
     Sends a summary notification of the day.
     Meant to be sent at a particular time, like at the end of a working day (e.g. 5pm).
     """
-    # TODO: load categories from data
-    top_categories = ["All"] + [k[0] for k, _ in CATEGORIES]
-    cat_time = get_time()
-    time_spent = [cat_time.get(c, timedelta()) for c in top_categories]
-    msg = f"Time spent today: {to_hms(time_spent[0])}\n"
-    msg += "Categories:\n"
-    msg += "\n".join(
-        f" - {c if c else 'All'}: {to_hms(t)}"
-        for c, t in sorted(
-            zip(top_categories, time_spent), key=lambda x: x[1], reverse=True
-        )
-        if t
+    categories = list(
+        set(["All", "Uncategorized"] + [">".join(k) for k, _ in CATEGORIES])
     )
-    notify("Checkin", msg)
+    cat_time = get_time(date=date)
+    time_spent = [cat_time.get(c, timedelta()) for c in categories]
+    top_categories = [
+        (c, to_hms(t))
+        for c, t in sorted(
+            zip(categories, time_spent), key=lambda x: x[1], reverse=True
+        )
+        if t > 0.02 * cat_time["All"]
+    ]
+    msg = ""
+    msg += "\n".join(f"- {c}: {t}" for c, t in top_categories)
+    if msg:
+        notify(title, msg)
+    else:
+        logger.debug("No time spent")
+
+
+def send_checkin_yesterday():
+    """Send a summary notification of yesterday, using `send_checkin`."""
+    yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+    send_checkin(title="Time yesterday", date=yesterday)
 
 
 @cache_ttl(60)
-def get_active_status() -> bool:
+def get_active_status() -> Union[bool, None]:
     """
     Get active status by polling latest event in aw-watcher-afk bucket.
     Returns True if user is active/not-afk, False if not.
@@ -359,7 +473,7 @@ def start_hourly():
                 logger.info("User is AFK, skipping hourly checkin")
                 continue
 
-            checkin()
+            send_checkin()
 
     threading.Thread(target=checkin_thread, daemon=True).start()
 
@@ -370,7 +484,7 @@ def start_new_day():
     """
 
     def new_day_thread():
-        last_day = None
+        last_day = (datetime.now(timezone.utc) - TIME_OFFSET).date()
         while True:
             now = datetime.now(timezone.utc)
             day = (now - TIME_OFFSET).date()
@@ -394,6 +508,13 @@ def start_new_day():
             sleep(60)
 
     threading.Thread(target=new_day_thread, daemon=True).start()
+
+
+def start_welcome_back():
+    """
+    Start a thread that sends a notification when the user becomes active after a period of non-activity.
+    """
+    # TODO
 
 
 if __name__ == "__main__":
